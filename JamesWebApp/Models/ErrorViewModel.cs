e6b111d4f87637e31e6bce99b780d45e0fb6fe77
James VanMeter <jamesvanmeter1219@gmail.com>
2022-11-11 21:26:46 -0500

    Add project files.

---
+++ JamesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JamesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
